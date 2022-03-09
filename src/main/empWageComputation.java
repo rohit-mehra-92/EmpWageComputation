4e845e870e05313b7cb54dec3d733c01e25d2aba
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-10 00:25:22 +0530

    first commit

--- src/main/empWageComputation.java
+++ src/main/empWageComputation.java
@@ -1,13 +1,7 @@
 package main;
 
-public class EmpWageBuilderUC1 {
-
+public class empWageComputation {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
-            System.out.println("employee is present");
-        else
-            System.out.println("employee is absent");
+        System.out.println("Welcome to Employee Wage Computation Program");
     }
 }
