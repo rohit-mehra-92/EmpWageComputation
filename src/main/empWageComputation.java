0f076f9ce842aa8d9edc7b1e4c9c46b928f439cd
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-10 00:31:14 +0530

    employee attendence

--- src/main/empWageComputation.java
+++ src/main/empWageComputation.java
@@ -1,7 +1,13 @@
 package main;
 
-public class empWageComputation {
+public class EmpWageBuilderUC1 {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("employee is present");
+        else
+            System.out.println("employee is absent");
     }
 }
