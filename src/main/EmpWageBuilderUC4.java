dcbe4aacfec2dfd234267faf321bfac9d9f1e4f3
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-10 21:17:57 +0530

    calculating employee wage using switch case

--- src/main/EmpWageBuilderUC4.java
+++ src/main/EmpWageBuilderUC4.java
@@ -1,36 +1,34 @@
 package main;
 
 public class EmpWageBuilderUC4 {
+    public static final int fullDay = 2;
+    public static final int halfDay = 1;
+    public static final int WagePerHr = 20;
+
     public static void main(String[] args) {
+        int totalWage;
+        int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 
-        int fullDay = 2;
-        int halfDay = 1;
-        int absent = 0;
-        int WagePerHr = 20;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-
-        switch (empCheck){
-        case fullDay:
+        switch (empCheck) {
+            case fullDay:
                 System.out.println("employee worked full day");
-                int workHour = 16;
-                int totalWage;
-                totalWage = (workHour * WagePerHr);
-                System.out.println("total wage is :$" + totalWage);
-
+                int fullDayWorkHour = 16;
+                totalWage = (fullDayWorkHour * WagePerHr);
+                System.out.println("total wage is : $" + totalWage);
+                break;
             case halfDay:
                 System.out.println("employee worked half day");
-                int workHour = 8;
-                int totalWage;
-                totalWage = (workHour * WagePerHr);
-                System.out.println("total wage is $:" + totalWage);
-
-            case absent:
+                int HalfDayWorkHour = 8;
+                totalWage = (HalfDayWorkHour * WagePerHr);
+                System.out.println("total wage is : $" + totalWage);
+                break;
+            default:
                 System.out.println("employee is absent");
                 int Wage = 0;
                 System.out.println("Total wage is : $" + Wage);
                 break;
 
-            }
         }
-
     }
+
+}
