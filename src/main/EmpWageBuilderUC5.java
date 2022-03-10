ee6659c3f7ab88fb0edee0c693b36a0d0756acfb
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-10 21:38:05 +0530

    Added employee total monthly wage

--- src/main/EmpWageBuilderUC5.java
+++ src/main/EmpWageBuilderUC5.java
@@ -1,9 +1,10 @@
 package main;
 
-public class EmpWageBuilderUC4 {
+public class EmpWageBuilderUC5 {
     public static final int fullDay = 2;
     public static final int halfDay = 1;
-    public static final int WagePerHr = 20;
+    public static final int Wage_Per_Hr = 20;
+    public static final int total_Working_Day=20;
 
     public static void main(String[] args) {
         int totalWage;
@@ -13,19 +14,19 @@ public class EmpWageBuilderUC4 {
             case fullDay:
                 System.out.println("employee worked full day");
                 int fullDayWorkHour = 16;
-                totalWage = (fullDayWorkHour * WagePerHr);
-                System.out.println("total wage is : $" + totalWage);
+                totalWage = (fullDayWorkHour * Wage_Per_Hr * total_Working_Day);
+                System.out.println("Monthly total wage is : $" + totalWage);
                 break;
             case halfDay:
                 System.out.println("employee worked half day");
                 int HalfDayWorkHour = 8;
-                totalWage = (HalfDayWorkHour * WagePerHr);
-                System.out.println("total wage is : $" + totalWage);
+                totalWage = (HalfDayWorkHour * Wage_Per_Hr * total_Working_Day);
+                System.out.println("Monthly total wage is : $" + totalWage);
                 break;
             default:
                 System.out.println("employee is absent");
                 int Wage = 0;
-                System.out.println("Total wage is : $" + Wage);
+                System.out.println("monthly total wage is : $" + Wage);
                 break;
 
         }
